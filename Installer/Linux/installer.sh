eb8456640f7fc0336e85ef621f0e22bce2a33720
Carpentini <327650783+Carpento@users.noreply.github.com>
2026-09-18 22:01:42 +0200

    Add files via upload

---
+++ Installer/Linux/installer.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+banner="What are you using:
+1) Linux 
+2) Termux
+"
+echo "$banner"
+read -p "--> " usr_ch
+
+if [[ $usr_ch = 1 ]]; then
+  username=$USER
+  sudo apt update -y
+  sudo apt upgrade -y 
+  sudo apt autoremove -y
+  sudo apt install openjdk git -y
+  java -version
+  cd /home/$username/desktop || exit
+elif [[ $usr_ch = 2 ]]; then
+  pkg update -y
+  apt upgrade -y
+  apt autoremove -y
+  pkg install openjdk git
+  java -version
+  cd ~ || exit
+else
+  echo "Invalid choice. Exiting."
+  exit 1
+fi
+
+cat > installer.sh <<EOF
+while true; do
+  echo -e "Select option\n1) Install\n2) Update\n3) Remove\n4) Close"
+  read -p "--> " user_choice
+  case \$user_choice in
+    1)
+      git clone https://github.com/Carpento/LHE-Local_Host_Executer.git
+      echo -e "\\nLHE installed successfully!\\n"
+      ;;
+    2)
+      rm -rfv LHE-Local_Host_Executer
+      echo -e "\\nRemoved old LHE-Local_Host_Executer\\n"
+      git clone https://github.com/Carpento/LHE-Local_Host_Executer.git
+      echo -e "\\nUpdated git LHE-Local_Host_Executer!\\n"
+      ;;
+    3)
+      rm -rfv LHE-Local_Host_Executer
+      echo -e "\\nRemoved LHE-Local_Host_Executer successfully!\\n"
+      ;;
+    4)
+      echo -e "\\nThanks for using Carpento's LHE installer\\n"
+      break
+      ;;
+    *)
+      echo -e "\\nWrong option\\n"
+      ;;
+  esac
+  read -p "Press Enter to continue"
+  clear
+done
+EOF
+
+chmod +x installer.sh
+./installer.sh
---
+++ Installer/Windows/installer.bat
@@ -0,0 +1,78 @@
+@echo off
+
+rem Check if Java is installed
+java -version >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Java is already installed.
+    java -version
+) else (
+    echo Java is not installed. Installing OpenJDK...
+    powershell Invoke-WebRequest https://download.oracle.com/java/22/latest/jdk-22_windows-x64_bin.exe -OutFile "C:\users\%username%\Desktop\openjdk-installer.exe"
+    cd /d "C:\users\%username%\Desktop" 
+    start /wait openjdk-installer.exe
+    echo OpenJDK installation completed.
+    java -version
+    cls
+    del /f "C:\users\%username%\Desktop\openjdk-installer.exe"
+)
+
+rem Check if Git is installed
+git --version >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Git is already installed
+) else (
+    echo Git is not installed. Installing Git...
+    powershell Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.45.2.windows.1/Git-2.45.2-64-bit.exe -OutFile "C:\users\%username%\Desktop\git-installer.exe"
+    cd /d "C:\users\%username%\Desktop"
+    start /wait git-installer.exe 
+    git --version
+    cls
+    del /f "C:\users\%username%\Desktop\git-installer.exe"
+)
+
+:installer
+cls
+echo Installer menu
+echo 1) Install
+echo 2) Update
+echo 3) Remove
+echo 4) Exit
+echo.
+
+set /P user_choice="--> "
+
+if "%user_choice%"=="1" (
+    git clone https://github.com/Carpento/Local_Host_Executer.git 
+    echo.
+    echo LHE-Local_Host_Executer installed successfully!
+    echo.
+    echo Thanks for using Carpento's LHE installer
+    echo.
+    set /p WAIT="Press enter to continue"
+) else if "%user_choice%"=="2" (
+    rmdir /s /q LHE-Local_Host_Executer
+    echo Old LHE-Local_Host_Executer removed
+    git clone https://github.com/Carpento/Local_Host_Executer.git
+    echo.
+    echo LHE-Local_Host_Executer updated successfully!
+    echo.
+    echo Thanks for using Carpento's LHE installer
+    echo.
+    set /p WAIT="Press enter to continue"
+) else if "%user_choice%"=="3" (
+    rmdir /s /q LHE-Local_Host_Executer
+    echo.
+    echo LHE-Local_Host_Executer removed successfully!
+    echo.
+    echo Thanks for using Carpento's LHE installer
+    echo.
+    set /p WAIT="Press enter to continue"
+) else if "%user_choice%"=="4" (
+    echo.
+    echo Thanks for using Carpento's LHE installer
+    echo.
+    timeout /t 2 /nobreak >nul
+    exit
+)
+
+goto installer
